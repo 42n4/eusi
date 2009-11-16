72f9c1b53064b43ed273388235302b01e28b735f
pwas <pwas@localhost.(none)>
2009-11-16 15:40:20 +0000

    Rpart

--- isowitdatasets.r
+++ isowitdatasets.r
@@ -3,7 +3,7 @@
 #te ¶cie¿ki mypath i mypathout musz± istnieæ w systemie
 #to ¶cie¿ka do plików wykonywalnych z R pow³oki za pomoc± source("...")
 #mypath<-"/media/disk/guest/"
-mypath<-"/home/pwas/workspace/iso/"
+mypath<-"/home/pwas/workspace/pwasiewi/"
 #to ¶cie¿ka do plików graficznych uzyskiwanych za pomoc± funkcji plot i innych
 mypathout<-paste(mypath,"obrazki/",sep="")
 
@@ -42,12 +42,12 @@ DataSetd<-disc.for.chosen(DataSet,parvec,3)
 #generujê z daisy ró¿nice miêdzy wierszami podanego zbioru i wprowadzam do isoMDS rzutuj±cego na dwa wymiary k=2 
 #(jak siê pojawi± dwa takie same wiersze to wyrzuca b³±d, dlatego na samym pocz±tku usuwa³em wiersze z Glass)
 #parvecfact=c("Type")
-parvecfact=c()
+if(nData=="Glass") parvecfact=c()
 parvec=setdiff(names(DataSet),parvecfact)
 nDataSets<-KruskelMDS(DataSet, parvec, 2)
 
 # uzyskany zbiór punktów kolorujê ró¿nymi atrybutami podstawianymi do wzorzec i uzyskujê wizualizacjê cech
-parvec<-names(DataSet)
+if(nData=="Glass") parvec<-names(DataSet)
 fname<-paste(mypathout,nData,"_norm_nrmdis",sep="")
 wzorzec1=DataSet$Type
 #pdm=1:length(unique(DataSet$Type))
@@ -57,11 +57,21 @@ plotMDS.for.chosen(fname, nDataSets, DataSetd, parvec, wzorzec1)
 
 
 #parvecfact=c("Type")
-parvecfact=c()
+if(nData=="Glass") parvecfact=c()
 parvec=setdiff(names(DataSet),parvecfact)
 nDataSets<-KruskelMDS(DataSet, parvec, 2)
 
-parvec<-names(DataSet)
+if(nData=="Glass") parvec<-names(DataSet)
 fname<-paste(mypathout,nData,"_norm_zscdis",sep="")
 wzorzec1=DataSet$Type
 plotMDS.for.chosen(fname, nDataSets, DataSetzd, parvec, wzorzec1) 
+
+t01<-rpart(Type~RI+Na+Mg+Al+Si+K+Ca+Ba+Fe,data=DataSet)
+zapisz_rpart(t01,paste(mypathout,nData,"_rpart_nrm"))
+t01<-rpart(Type~RI+Na+Mg+Al+Si+K+Ca+Ba+Fe,data=DataSetz)
+zapisz_rpart(t01,paste(mypathout,nData,"_rpart_zsc"))
+
+
+
+
+
