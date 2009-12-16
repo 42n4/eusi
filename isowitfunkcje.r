4c7d9b2d46422cdd3c926269abd30328ebd6c393
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2009-12-16 22:09:51 +0000

    Dodano pakiet mlr

--- isowitfunkcje.r
+++ isowitfunkcje.r
@@ -9,7 +9,7 @@ Sys.setlocale("LC_NUMERIC","C")
 #lista pakietów z CRAN-u
 #naprawiæ pakier gRain
 #pkglist<-c("gRain")
-pkglist<-c("rsprng","bnlearn","snow","ggplot2","reshape","gbm","caret","arules","mboost","bestglm","ElemStatLearn","faraway","relaimpo","leaps","lars","bootstrap","DAAG","ff","biglm","bigmemory","splines","betareg","ellipse","nlme","MASS","leaps","car","lmtest","gregmisc","foreign","plyr","mlbench","boot","Hmisc","RWeka","ipred","klaR","ROCR","rpart","dprep","maptree","party","grid","lattice","latticeExtra","playwith","ada","randomForest","kknn","e1071","cluster","class","caret","fda","zoo","lattice","deal","RJDBC","cairoDevice")
+pkglist<-c("snowfall","rsprng","bnlearn","snow","ggplot2","reshape","gbm","caret","arules","mboost","bestglm","ElemStatLearn","faraway","relaimpo","leaps","lars","bootstrap","DAAG","ff","biglm","bigmemory","splines","betareg","ellipse","nlme","MASS","leaps","car","lmtest","gregmisc","foreign","plyr","mlbench","boot","Hmisc","RWeka","ipred","klaR","ROCR","rpart","dprep","maptree","party","grid","lattice","latticeExtra","playwith","ada","randomForest","kknn","e1071","cluster","class","caret","fda","zoo","lattice","deal","RJDBC","cairoDevice")
 pkgcheck <- pkglist %in% row.names(installed.packages())
 for(i in pkglist[!pkgcheck]){
 	install.packages(i,depend=TRUE)
@@ -22,6 +22,12 @@ for(i in biolist[!biocheck]){
     source("http://bioconductor.org/biocLite.R")
 	#update.packages(repos=biocinstallRepos(), ask=FALSE)
 	biocLite(i)
+}
+
+pkgrforge<-c("mlr")
+pkgcheck <- pkgrforge %in% row.names(installed.packages())
+for(i in pkglist[!pkgcheck]){
+	install.packages("mlr", repos="http://R-Forge.R-project.org")
 }
 
 for(i in pkglist)
