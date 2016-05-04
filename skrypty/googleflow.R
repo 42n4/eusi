fdda3758cfcf16298a910cc8efe314c7f9a0c6b1
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2016-05-05 00:31:22 +0200

    doc wsteczna propagacja

--- skrypty/googleflow.R
+++ skrypty/googleflow.R
@@ -1,1 +1,5 @@
 https://github.com/terrytangyuan/rflow
+
+http://shitalshah.com/p/downloading-all-of-hacker-news-posts-and-comments/
+https://github.com/fhoffa/notebooks/blob/master/analyzing%20hacker%20news.ipynb
+
