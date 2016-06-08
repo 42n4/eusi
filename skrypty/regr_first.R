ee230ebc40976d2e3f32dc51c6bbf563d0735b7c
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2016-06-08 16:25:02 +0200

    Regresja spolszczona - materiały pomocnicze do wykładu

--- skrypty/regr_first.R
+++ skrypty/regr_first.R
@@ -11,22 +11,22 @@
 # SPADEK GRADIENTU - w 1D pochodnej - dla funkcji 1D
 
 funkcja <- function(x) {                     # zdefiniuj funkcję 1D z optimum w punkcie (0,0)
-  x[1]^2
+  x^2
 }
 
 pochodna <- function(x) {                    # określ pochodną funkcji 1D
-  2*x[1]
+  2*x
 }
 
 # metoda spadku gradientu w 1D
-spadek_gradientu <- function(func, pochodna, start, step=0.05, tol=1e-8) {
+spadek_gradientu <- function(func, pochodna, start, alfa=0.05, tol=1e-8) {
   pt1 <- start                               # start
   grdnt <- pochodna(pt1)                     # pochodna funkcji 
-  pt2 <- pt1[1] - step*grdnt[1]              # odejmujemy pochodną razy step od wejściowego parametru
+  pt2 <- pt1 - alfa*grdnt                    # odejmujemy pochodną razy alfa od wejściowego parametru
   while (abs(func(pt1)-func(pt2)) > tol) {   # przy minimum małe wartości pochodnej, jeśli mniejsze od tol przerwij pętle
     pt1 <- pt2                               # w przypadku spadku funkcji dodajemy do x
     grdnt <- pochodna(pt1)                   # w przypadku wzrostu funkcji odejmujemy do x
-    pt2 <- pt1[1] - step*grdnt[1]            # aby "stoczyć się" do minimum
+    pt2 <- pt1 - alfa*grdnt                  # aby "stoczyć się" do minimum
     print(func(pt2))                         # wypisuj postęp schodzenia do minimum
   }                                          
   pt2                                        # zwróć ostatni punkt optymalizacji
@@ -43,7 +43,7 @@ result <- spadek_gradientu(                  # znajdź minimum używając funkcj
 print(result)                                # współrzędne minimum
 print(funkcja(result))                       # wartość funkcji w minimum
 x <- seq(-3, 3, length.out=100)
-y <- sapply(x, funkcja)
+y <- sapply(x, funkcja)                      # inaczej y <- funkcja(x)
 plot(x, y, xlab="x",ylab="y")                # wyswietl funkcję 1D na wykresie
 # wskaż czerwonym punktem znalezione optimum
 points(result, funkcja(result), col="red", pch=19)
@@ -62,14 +62,14 @@ gradient <- function(x) {                    # określ pochodną funkcji 2D
 }
 
 # metoda spadku gradientu w 2D
-spadek_gradientu <- function(func, gradient, start, step=0.05, tol=1e-8) {
+spadek_gradientu <- function(func, gradient, start, alfa=0.05, tol=1e-8) {
   pt1 <- start                               # start
   grdnt <- gradient(pt1)                     # gradient funkcji po przecieciu wzdłuż x i y
-  pt2 <- c(pt1[1] - step*grdnt[1], pt1[2] - step*grdnt[2])
+  pt2 <- c(pt1[1] - alfa*grdnt[1], pt1[2] - alfa*grdnt[2])
   while (abs(func(pt1)-func(pt2)) > tol) {   # przy minimum małe wartości gradientu, jeśli mniejsze od tol przerwij pętle
     pt1 <- pt2                               # w przypadku spadku funkcji przecięcia (część gradientu) dodajemy do odpowiedniego wymiaru
     grdnt <- gradient(pt1)                   # w przypadku wzrostu funkcji przecięcia odejmujemy od odpowiedniego wymiaru
-    pt2 <- c(pt1[1] - step*grdnt[1], pt1[2] - step*grdnt[2])
+    pt2 <- c(pt1[1] - alfa*grdnt[1], pt1[2] - alfa*grdnt[2])
     print(func(pt2))                         # wypisuj postęp schodzenia do minimum
   }
   pt2                                        # zwróć ostatni punkt optymalizacji
@@ -111,12 +111,12 @@ abline(res, col='blue')
 Sys.sleep(2)                                 #pauza na 2 sekundy
 
 ################################################################################
-# Funkcja kosztów - kwadraty błędów
+# Funkcja kosztów - suma kwadratów błędów między y, a krzywą regresji: y - (Ax+B)
 cost <- function(X, y, theta) {
   sum( (X %*% theta - y)^2 ) / (2*length(y))
 }
 
-alpha <- 0.01                                # współczynnik uczenia alfa
+alfa <- 0.01                                 # współczynnik uczenia alfa
 num_iters <- 1000                            # liczba iteracji
 cost_history <- double(num_iters)            # historia
 theta_history <- list(num_iters)
@@ -126,7 +126,7 @@ X <- cbind(1, matrix(x))                     # dodaj jedynki dla B z Ax+B wzoru 
 for (i in 1:num_iters) {                     # spadek gradientu dla sumy kwadratów różnic po osi y
   error <- (X %*% theta - y)                 # między punktami (x,y) a linią regresji A*X+B 
   delta <- t(X) %*% error / length(y)        # gdzie Theta to wektor [B,A]
-  theta <- theta - alpha * delta             # zwiększamy lub zmniejszamy theta 
+  theta <- theta - alfa * delta              # zwiększamy lub zmniejszamy theta 
   cost_history[i] <- cost(X, y, theta)       # w zależności od wartości gradientu w delta  
   theta_history[[i]] <- theta                # notujemy historię kosztów i theta
 }
